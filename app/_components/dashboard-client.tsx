88606f55e4c246382a6c42daa05d026e6000339a
Azar <79351619+azr-arch@users.noreply.github.com>
2024-12-15 19:28:05 +0530

    dashboard client fix

--- app/_components/dashboard-client.tsx
+++ app/_components/dashboard-client.tsx
@@ -8,6 +8,7 @@ import { LogOut } from "lucide-react";
 import Image from "next/image";
 
 export const DashboardClient = ({ data }: { data?: any }) => {
+    console.log({ data });
     return (
         <div>
             <div className="container mx-auto px-4 py-8 border-t border-neutral-400">
@@ -18,50 +19,53 @@ export const DashboardClient = ({ data }: { data?: any }) => {
                     </Button>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                    {data.map((article: any) => (
-                        <Card key={article.article_id} className="flex flex-col">
-                            <CardHeader>
-                                <CardTitle className="text-lg line-clamp-2">
-                                    {article.title}
-                                </CardTitle>
-                            </CardHeader>
-                            {article.image_url && (
-                                <Image
-                                    src={article.image_url}
-                                    alt={article.title}
-                                    width={100}
-                                    height={40}
-                                    className="w-full  object-cover"
-                                />
-                            )}
-                            <CardContent className="flex-grow py-2">
-                                <p className="text-sm text-gray-600 mb-2 line-clamp-3">
-                                    {article.description}
-                                </p>
-                                <div className="flex flex-wrap gap-2 mb-2">
-                                    {article.category.map((cat: any) => (
-                                        <Badge key={cat} variant="secondary">
-                                            {cat}
-                                        </Badge>
-                                    ))}
-                                </div>
-                                <p className="text-xs text-gray-500">
-                                    Published: {new Date(article.pubDate).toLocaleDateString()}
-                                </p>
-                            </CardContent>
-                            <CardFooter>
-                                <Button asChild className="w-full">
-                                    <a
-                                        href={article.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        Read More
-                                    </a>
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    ))}
+                    {!data && <p className="">{"Articles not found"}</p>}
+                    {data &&
+                        data?.length > 0 &&
+                        data.map((article: any) => (
+                            <Card key={article.article_id} className="flex flex-col">
+                                <CardHeader>
+                                    <CardTitle className="text-lg line-clamp-2">
+                                        {article.title}
+                                    </CardTitle>
+                                </CardHeader>
+                                {article.image_url && (
+                                    <Image
+                                        src={article.image_url}
+                                        alt={article.title}
+                                        width={100}
+                                        height={40}
+                                        className="w-full  object-cover"
+                                    />
+                                )}
+                                <CardContent className="flex-grow py-2">
+                                    <p className="text-sm text-gray-600 mb-2 line-clamp-3">
+                                        {article.description}
+                                    </p>
+                                    <div className="flex flex-wrap gap-2 mb-2">
+                                        {article.category.map((cat: any) => (
+                                            <Badge key={cat} variant="secondary">
+                                                {cat}
+                                            </Badge>
+                                        ))}
+                                    </div>
+                                    <p className="text-xs text-gray-500">
+                                        Published: {new Date(article.pubDate).toLocaleDateString()}
+                                    </p>
+                                </CardContent>
+                                <CardFooter>
+                                    <Button asChild className="w-full">
+                                        <a
+                                            href={article.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Read More
+                                        </a>
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
                 </div>
             </div>
         </div>
